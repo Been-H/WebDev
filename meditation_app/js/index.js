b8c1cb268e8077d4a6a7b56104511ddf2b44fd9b
Ben Hodges <71901668+Been-H@users.noreply.github.com>
2021-05-06 20:10:05 -0400

    Add files via upload

---
+++ meditation_app/js/index.js
@@ -0,0 +1,39 @@
+var buttons = [document.querySelector("#feature-link"),
+    document.querySelector("#testimonials-link"),
+    document.querySelector("#plans-link")
+];
+
+var destinations = [
+    document.querySelector(".section-features"),
+    document.querySelector(".section-testimonials"),
+    document.querySelector(".section-plans")
+];
+
+function smoothScroll(target, duration) {
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        console.log(run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        return c * Math.sin(t / d * (Math.PI / 2)) + b;
+    };
+
+
+    requestAnimationFrame(animation);
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        smoothScroll(destinations[i], 1300)
+    });
+}
